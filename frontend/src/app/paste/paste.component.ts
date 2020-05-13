f6e83e947578087681c43a512a14192744ec7c08
matthewaj <matthewaj13@gmail.com>
2020-05-12 23:37:17 -0500

    Implement Paste Component Logic
    - Added the ability to create a new paste
    - Added the ability to view an existing paste.

--- frontend/src/app/paste/paste.component.ts
+++ frontend/src/app/paste/paste.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Location} from '@angular/common';
+
+const API_URL  = 'http://localhost:4200/api/pastes/';
+
+interface response {
+  id: string,
+  name: string,
+  text: string,
+}
+
+interface Form {
+  name: string,
+  title: string
+}
 
 @Component({
   selector: 'app-paste',
@@ -6,10 +23,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paste.component.css']
 })
 export class PasteComponent implements OnInit {
+  private pasteID = null;
+  public error = false;
+  public response: response = null;
+  public createPasteForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpClient: HttpClient,
+    private formBuilder: FormBuilder,
+    private location: Location
+  ) {
+    this.createPasteForm = this.formBuilder.group({
+      text: '',
+      name: '',
+    });
   }
 
+  // Todo: Add Error Handling
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.pasteID = params.id === undefined ? null : params.id;
+
+      if (this.pasteID !== null) {
+        this.httpClient.get<response>(API_URL + this.pasteID)
+          .subscribe(results => {
+            this.response = results
+          })
+      }
+    })
+  }
+
+  // Todo: Add Error Handling
+  addPaste(form: Form) {
+    this.httpClient.post<response>(API_URL, form)
+      .subscribe(results => {
+        this.response = results;
+        this.location.go(`/${results.id}`)
+      })
+  }
+
+  onSubmit(value: Form) {
+    this.addPaste(value);
+    this.createPasteForm.reset()
+  }
 }
